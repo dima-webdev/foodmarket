fb9315c48cf6a13e06f01df2de332c3d57abfc28
dima-webdev <nguyendmitry@gmail.com>
2019-07-04 23:09:06 +0300

    Updates

---
+++ precache-manifest.2b3cc45a3ab85ca48fc8817b04ef7d88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc218debe5f56d88c774eaac29b3d174",
+    "url": "/foodmarket/index.html"
+  },
+  {
+    "revision": "edac6e4ed47b326251ae",
+    "url": "/foodmarket/static/css/main.9442e29b.chunk.css"
+  },
+  {
+    "revision": "16a6385f42917bcffd60",
+    "url": "/foodmarket/static/js/2.71412b09.chunk.js"
+  },
+  {
+    "revision": "edac6e4ed47b326251ae",
+    "url": "/foodmarket/static/js/main.7c1abc7e.chunk.js"
+  },
+  {
+    "revision": "fd19cadb2b5e551cffa9",
+    "url": "/foodmarket/static/js/runtime~main.9607f6bd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dima-webdev/foodmarket/precache-manifest.89f4534739c50e77fb4ad238706f2411.js"
+  "/foodmarket/precache-manifest.2b3cc45a3ab85ca48fc8817b04ef7d88.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dima-webdev/foodmarket/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/foodmarket/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
